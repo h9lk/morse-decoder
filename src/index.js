a8c0bb7a4a3f49e04e640c124ac08632fce7e228
”Vlad <hilk.2013@yandex.ru>
2022-01-20 14:54:12 +0300

    fix: structure

--- src/index.js
+++ src/index.js
@@ -11,30 +11,30 @@ function decode(expr) {
   let res = '';
 
   for (let i = 0; i < expr.length; i += 10) {
-    let letter = expr.slice(i, i + 10);
+    const letter = expr.slice(i, i + 10);
 
     if (letter === '**********') {
       res += ' ';
     } else {
       let morse = '';
 
-      for(let j = 0; j < 10; j += 2) {
+      for (let j = 0; j < 10; j += 2) {
         if (letter.slice(j, j + 2) === '10') {
-          morse += '.'
-        };
+          morse += '.';
+        }
 
         if (letter.slice(j, j + 2) === '11') {
-          morse += '-'
-        };
+          morse += '-';
+        }
       }
 
       res += MORSE_TABLE[morse];
     }
   }
 
-  return res;    
+  return res;
 }
 
 module.exports = {
-    decode
-}
+  decode,
+};
